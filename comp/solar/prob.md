cba4e2b3207e5a4e800c85d7deb3a5b52e3f7b4a
Jasper <40232406+jaspwr@users.noreply.github.com>
2024-06-02 18:19:33 +1000

    feat: implemented solar question

--- comp/solar/prob.md
+++ comp/solar/prob.md
@@ -1,5 +1,5 @@
 ```toml
-# TODO: Calify tilt relative to sun
+# TODO: Verify that generated answers are actually correct. 
 
 [fuzz]
 exec = ["cargo", "run", "--release", "--", "generate"]
@@ -10,7 +10,7 @@ exec = ["cargo", "run", "--release", "--", "validate"]
 ```
 
 # 🇦🇶Solar powered research station
-A remote Antarctic research station is being designed and solar cells are being considered as a method of powering the station. The station is set to be located at a **latitude of -76** (76° lower than the equator). The Earth's equator is on a **23.5° axial tilt** from it's orbital plane. Assume that the orbital inclination is 0°. A solar panel can be **rotated north and south by ±15° from pointing straight up** with motors but doing so costs **1 joule per degree** of rotation. The panel outputs $\alpha\cos(\theta)$ watts (joules per second) where $\theta$ is the angle between the panel and the sun and $\alpha$ is your question input. Find the maximum possible number of joules that can be generated in 1 day.
+A remote Antarctic research station is being designed and solar cells are being considered as a method of powering the station. The station is set to be located at a **latitude of -76** (76° lower than the equator). The Earth's equator is on a **23.5° axial tilt** from it's orbital plane. Assume that the orbital inclination is 0° and that the Earth's axis of rotation is leaning 23.5° directly away from the sun as shown in the diagram below. A solar panel can be **rotated north and south by ±15° from pointing straight up** with motors but doing so costs **1 joule per degree** of rotation. The panel outputs $\alpha\cos(\theta)$ watts (joules per second) where $\theta$ is the angle between the panel and the sun and $\alpha$ is your question input. Find the maximum possible number of joules that can be generated in 1 day within ±0.5 joules.
 
 ![diagram](diagram.png)
 
--- comp/solar/src/main.rs
+++ comp/solar/src/main.rs
@@ -17,7 +17,7 @@ fn main() {
 
     let mut rng = StdRng::seed_from_u64(seed);
 
-    let alpha = rng.gen_range(0.1..0.5);
+    let alpha = rng.gen_range(1.0..5.0);
 
     match args[1].as_str() {
         "generate" => {
@@ -32,9 +32,9 @@ fn main() {
                 .parse::<f64>()
                 .expect("Invalid input. Expected number.");
 
-            let solution: f64 = todo!();
+            let solution: f64 = simulate(alpha);
 
-            if (input - solution).abs() < 0.01 {
+            if (input - solution).abs() < 0.5 {
                 exit(0);
             } else {
                 exit(1);
@@ -42,4 +42,114 @@ fn main() {
         }
         _ => panic!(),
     }
+}
+
+struct Vec3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Vec3 {
+    fn dot(&self, other: &Vec3) -> f64 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    fn mag(&self) -> f64 {
+        (self.x.powi(2) + self.y.powi(2) + self.z.powi(2)).sqrt()
+    }
+
+    fn angle_between(&self, other: &Vec3) -> f64 {
+        self.dot(other) / (self.mag() * other.mag())
+    }
+}
+
+struct Angle {
+    lat: f64,
+    long: f64,
+}
+
+impl Angle {
+    fn to_vec3(&self) -> Vec3 {
+        Vec3 {
+            x: self.lat.cos() * self.long.cos(),
+            y: self.lat.cos() * self.long.sin(),
+            z: self.lat.sin(),
+        }
+    }
+
+    fn angle_between(&self, other: &Angle) -> f64 {
+        self.to_vec3().angle_between(&other.to_vec3())
+    }
+}
+
+const SUN_LAT: f64 = -23.5_f64 * (std::f64::consts::PI / 180.0);
+const STATION_LAT: f64 = -76.0_f64 * (std::f64::consts::PI / 180.0);
+
+/// Power output in watts
+fn power(alpha: f64, time: f64, tilt: f64) -> f64 {
+    assert!(tilt.abs() <= 15.0_f64.to_radians());
+
+    let pointing = Angle {
+        lat: STATION_LAT + tilt,
+        long: long_of_time(time),
+    };
+
+    let sun = Angle {
+        lat: SUN_LAT,
+        long: 0.0,
+    };
+
+    let angle = sun.angle_between(&pointing);
+
+    if angle < 0.0 {
+        return 0.0;
+    }
+
+    alpha * angle
+}
+
+const SECONDS_IN_DAY: f64 = 86400.0;
+
+fn long_of_time(time: f64) -> f64 {
+    (time / SECONDS_IN_DAY) * 2.0 * std::f64::consts::PI
+}
+
+/// Optimal tilt in radians
+/// Does not account for power lost to motor just finds angle that maximises power output
+fn optimise_tilt(alpha: f64, time: f64) -> f64 {
+    let mut best_tilt = 0.0;
+    let mut best_power = 0.0;
+
+    for tilt in (-100..100).map(|x| x as f64) {
+        let tilt = tilt as f64 / 100. * 15.0_f64.to_radians();
+
+        let power = power(alpha, time, tilt);
+
+        if power > best_power {
+            best_power = power;
+            best_tilt = tilt;
+        }
+    }
+
+    best_tilt
+}
+
+fn simulate(alpha: f64) -> f64 {
+    let mut total_energy = 0.0;
+
+    let mut previous_motor_tilt: f64 = 0.0;
+
+    for time in 0..SECONDS_IN_DAY as i32 {
+        let time = time as f64;
+
+        let motor_tilt: f64 = optimise_tilt(alpha, time);
+
+        total_energy += power(alpha, time, motor_tilt);
+        total_energy -= (motor_tilt - previous_motor_tilt).abs();
+
+        previous_motor_tilt = motor_tilt;
+    }
+
+    total_energy
 }
