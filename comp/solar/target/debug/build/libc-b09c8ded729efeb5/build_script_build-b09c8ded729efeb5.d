53004ddf9c2358aca8e86a91289f10267e55d3f9
Jasper <40232406+jaspwr@users.noreply.github.com>
2024-06-07 18:43:45 +1000

    fix: remove solar/target

--- comp/solar/target/debug/build/libc-b09c8ded729efeb5/build_script_build-b09c8ded729efeb5.d
+++
@@ -1,5 +0,0 @@
-/home/jasper/dev/ProgComp_rebase/ProgComp2024/comp/solar/target/debug/build/libc-b09c8ded729efeb5/build_script_build-b09c8ded729efeb5: /home/jasper/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.155/build.rs
-
-/home/jasper/dev/ProgComp_rebase/ProgComp2024/comp/solar/target/debug/build/libc-b09c8ded729efeb5/build_script_build-b09c8ded729efeb5.d: /home/jasper/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.155/build.rs
-
-/home/jasper/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.155/build.rs:
